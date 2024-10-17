305e4bf7465158f412c57d627383d212c3c549d3
LinRF <2673760057@qq.com>
2024-10-17 10:13:02 +0800

    引入Pinia

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router/router'
 
@@ -6,10 +7,12 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import Request from './utils/Request'
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.config.globalProperties.Request = Request
 
 app.use(router)
 app.use(ElementPlus)
+app.use(pinia)
 app.mount('#app')
